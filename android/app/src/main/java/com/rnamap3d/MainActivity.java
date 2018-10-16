add3df88bab3f407c018341f1694daa6ab379d63
duyang29 <youngdu29@gmail.com>
2018-10-16 18:40:35 +0800

    fix rn 0.57.1 cannot get location

--- android/app/src/main/java/com/rnamap3d/MainActivity.java
+++ android/app/src/main/java/com/rnamap3d/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "RNAMap3D";
+        return "rnamap3d";
     }
 }
